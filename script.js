2fc97367d7cc01b8bd874a91d5965962c603ff5f
saikat <saikat@macs-MacBook-Pro.local>
2025-02-24 20:31:01 +0600

    hh

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-});
+
 
     // Mobile sidebar toggle
     const sidebar = document.querySelector(".sidebar");
